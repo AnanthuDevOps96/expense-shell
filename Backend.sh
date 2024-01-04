67094e92b05bc149541c3aa6f41aac255b21a482
AnanthuDevOps96 <ananthu.r.prasad@gmail.com>
2024-01-04 09:37:50 +0530

    Backend script is enabled

---
+++ Backend.sh
@@ -0,0 +1,22 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+dnf install nodejs -y
+
+useradd expense
+mkdir /App
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /App
+unzip /tmp/backend.zip
+cd /App
+npm install
+
+cp backend.service /etc/systemd/system/backend.service
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl restart backend
+
+dnf install mysql -y
+mysql -h 172.31.32.119 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
