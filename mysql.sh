8837d348f4e30ee888b289bb9b52ba2abd5a48f8
AnanthuDevOps96 <ananthu.r.prasad@gmail.com>
2024-01-04 09:26:43 +0530

    Mysql server script has been executed

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
-dnf install mysql-coummunit-server -y
+dnf install mysql-coummunity-server -y
 systemctl enable mysqld
 systemctl start mysqld
 mysql_secure_installation --set-root-pass ExpenseApp@1
