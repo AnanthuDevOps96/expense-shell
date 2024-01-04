faab6d6c9c4d23777a682fdddcc676616006ca42
AnanthuDevOps96 <ananthu.r.prasad@gmail.com>
2024-01-04 09:51:12 +0530

    Backend script is enabled

--- Backend.sh
+++ Backend.sh
@@ -8,14 +8,11 @@ mkdir /App
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /App
 unzip /tmp/backend.zip
-cd /App
 npm install
 
 cd
 
 cp backend.service /etc/systemd/system/backend.service
-
-cd
 
 systemctl daemon-reload
 systemctl enable backend
