672e50995bd880e101b3695a494a73cc8bcf1d53
AnanthuDevOps96 <ananthu.r.prasad@gmail.com>
2024-01-04 10:00:19 +0530

    Backend script is enabled

--- Backend.sh
+++ Backend.sh
@@ -3,14 +3,17 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 
-cp backend.service /etc/systemd/system/backend.service
-
 useradd expense
+rm -rf /App
 mkdir /App
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /App
 unzip /tmp/backend.zip
 npm install
+
+cd
+cp backend.service /etc/systemd/system/backend.service
+cd
 
 
 systemctl daemon-reload
